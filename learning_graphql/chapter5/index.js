24a1cdee99d7bd8806229f94f1c3b0fe86491b7d
longtime1116 <longtime1116@gmail.com>
2020-04-02 17:18:44 +0900

    カスタムスカラー型(command)

--- learning_graphql/chapter5/index.js
+++ learning_graphql/chapter5/index.js
@@ -43,7 +43,7 @@ input PostPhotoInput {
 type Query {
     totalPhotos: Int!
     allUsers: [User!]!
-    allPhotos: [Photo!]!
+    allPhotos(after: DateTime): [Photo!]!
 }
 type Mutation {
   postPhoto(input: PostPhotoInput): Photo!
@@ -116,7 +116,12 @@ const resolvers = {
   Query: {
     totalPhotos: () => photos.length,
     allUsers: () => users,
-    allPhotos: () => photos
+    allPhotos: (parent, args) => {
+      return photos.filter(
+        photo =>
+          new Date(photo.createdAt).getTime() > new Date(args.after).getTime()
+      );
+    }
   },
   Mutation: {
     // name と description を渡したらそれを登録する。
@@ -124,7 +129,8 @@ const resolvers = {
     postPhoto: (parent, args) => {
       var newPhoto = {
         id: _id++,
-        ...args.input
+        ...args.input,
+        createdAt: new Date()
       };
       photos.push(newPhoto);
       return newPhoto;
@@ -144,7 +150,7 @@ const resolvers = {
   Photo: {
     url: parent => `http://yoursite.com/img${parent.id}.jpg`,
     postedBy: parent => {
-      return users.find(u => u.githubLoginID === parent.githubUser);
+      return users.find(u => u.githubLoginID === parent.githubUser) || [];
     },
     //taggedUsers: []
     taggedUsers: parent => {
@@ -156,9 +162,9 @@ const resolvers = {
   DateTime: new GraphQLScalarType({
     name: `DateTime`,
     description: `A valid date time value`,
-    parseValue: value => new Date(value),
-    serialize: value => new Date(value).toISOString(),
-    parseLiteral: ast => ast.value
+    parseValue: value => new Date(value), // これでエラーにならないもののみがDateTime型とみなされる
+    serialize: value => new Date(value).toISOString(), // ISOString で返されるようにする
+    parseLiteral: ast => ast.value // クエリ引数を parse するのに使う
   })
 };
 
