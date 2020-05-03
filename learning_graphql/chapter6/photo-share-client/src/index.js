fdca8d873bd4b74e2f626c7153dd5b03750c96ce
longtime1116 <longtime1116@gmail.com>
2020-05-03 20:26:59 +0900

    use ApolloProvider

--- learning_graphql/chapter6/photo-share-client/src/index.js
+++ learning_graphql/chapter6/photo-share-client/src/index.js
@@ -1,32 +1,30 @@
-//import React from 'react';
-//import ReactDOM from 'react-dom';
-//import './index.css';
-//import App from './App';
-//import * as serviceWorker from './serviceWorker';
-//
-//ReactDOM.render(
-//  <React.StrictMode>
-//    <App />
-//  </React.StrictMode>,
-//  document.getElementById('root')
-//);
-//
-//// If you want your app to work offline and load faster, you can change
-//// unregister() to register() below. Note this comes with some pitfalls.
-//// Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
-
-import ApolloClient, { gql } from "apollo-boost";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({ uri: "http://localhost:4000/graphql" });
-const query = gql`
-  {
-    totalUsers
-    totalPhotos
-  }
-`;
-client
-  .query({ query })
-  .then(({ data }) => console.log("data", data))
-  //.then(() => console.log("cache", client.extract()))
-  .catch((error) => console.log(error));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
+
+// NOTE: try to use ApolloClient only
+//import ApolloClient, { gql } from "apollo-boost";
+//
+//const client = new ApolloClient({ uri: "http://localhost:4000/graphql" });
+//const query = gql`
+//  {
+//    totalUsers
+//    totalPhotos
+//  }
+//`;
+//client
+//  .query({ query })
+//  .then(({ data }) => console.log("data", data))
+//  //.then(() => console.log("cache", client.extract()))
+//  .catch((error) => console.log(error));
+//
