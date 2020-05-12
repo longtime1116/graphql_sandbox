b38f8c081bff617e9ff54499a973cc6f5444855b
longtime1116 <longtime1116@gmail.com>
2020-05-13 02:39:31 +0900

    use HTTP and WS as appropriate

--- learning_graphql/chapter6/photo-share-client/src/index.js
+++ learning_graphql/chapter6/photo-share-client/src/index.js
@@ -2,8 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { ApolloProvider } from "react-apollo";
-import ApolloClient, { InMemoryCache } from "apollo-boost";
+import {
+  InMemoryCache,
+  HttpLink,
+  ApolloLink,
+  ApolloClient,
+  split,
+} from "apollo-boost";
 import { persistCache } from "apollo-cache-persist";
+import { WebSocketLink } from "apollo-link-ws";
+import { getMainDefinition } from "apollo-utilities";
 
 const cache = new InMemoryCache();
 persistCache({
@@ -16,18 +24,36 @@ if (localStorage["apollo-cache-persist"]) {
   let cacheData = JSON.parse(localStorage["apollo-cache-persist"]);
   cache.restore(cacheData);
 }
-const client = new ApolloClient({
-  cache,
-  uri: "http://localhost:4000/graphql",
-  request: (operation) => {
-    operation.setContext((context) => ({
-      headers: {
-        ...context.headers,
-        authorization: localStorage.getItem("token"),
-      },
-    }));
-  },
+
+const httpLink = new HttpLink({ uri: "http://localhost:4000/graphql" });
+const wsLink = new WebSocketLink({
+  uri: "ws://localhost:4000/graphql",
+  options: { reconnect: true },
 });
+
+const authLink = new ApolloLink((operation, forward) => {
+  operation.setContext((context) => ({
+    headers: {
+      ...context.headers,
+      authorization: localStorage.getItem("token"),
+    },
+  }));
+  return forward(operation);
+});
+// http 通信をするときに authLink で authorization 情報を付与して実行できるようにする
+const httpAuthLink = authLink.concat(httpLink);
+
+// 第1引数に関数、trueならば第2引数を使い、falseならば第3引数を使う
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === "OperationDefinition" && operation === "subscription";
+  },
+  wsLink,
+  httpAuthLink
+);
+
+const client = new ApolloClient({ cache, link });
 ReactDOM.render(
   <ApolloProvider client={client}>
     {console.log(process.env)}
